b34e0eab34737c43bde34f3f130c2d95973f3c08
Kana <icontractor@atlassoftweb.com>
2017-10-30 19:43:01 +0530

    Verify Taplink api and set encryption available flag.

--- BlindHash/SecurePassword/Model/Observer/verifyTapLinkApi.php
+++ BlindHash/SecurePassword/Model/Observer/verifyTapLinkApi.php
@@ -6,27 +6,53 @@ class verifyTapLinkApi implements \Magento\Framework\Event\ObserverInterface
     protected $scopeConfig;
     protected $encryptor;
     protected $resourceConfig;
+    protected $messageManager;
 
-    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig, \Magento\Framework\Encryption\EncryptorInterface $encryptor, \Magento\Framework\App\Config\ConfigResource\ConfigInterface $resourceConfig)
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig, \Magento\Framework\Encryption\EncryptorInterface $encryptor, \Magento\Framework\App\Config\ConfigResource\ConfigInterface $resourceConfig, \Magento\Framework\Message\ManagerInterface $messageManager)
     {
         $this->scopeConfig = $scopeConfig;
         $this->encryptor = $encryptor;
         $this->resourceConfig = $resourceConfig;
+        $this->messageManager = $messageManager;
     }
 
+    /**
+     * Verify Tap link App Id and store public key
+     * 
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        if (!$this->scopeConfig->getValue('blindhash/general/enabled')) {
+        if (empty($this->scopeConfig->getValue('blindhash/general/api_key')))
             return;
-        }
+
+        if ($this->scopeConfig->getValue('blindhash/general/api_public_key'))
+            return;
+
 
         $taplink = $this->encryptor->getTaplinkObject();
         $publicKey = $taplink->getPublicKey();
-        
-        // TODO encrypt test
+        $encryptTest = false;
 
-        if ($publicKey) {
+        if (empty($publicKey)) {
+            $this->resourceConfig->saveConfig('blindhash/general/enabled', '', \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT, \Magento\Store\Model\Store::DEFAULT_STORE_ID);
+            $this->resourceConfig->saveConfig('blindhash/general/api_key', '', \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT, \Magento\Store\Model\Store::DEFAULT_STORE_ID);
+            $this->resourceConfig->saveConfig('blindhash/general/api_public_key', '', \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT, \Magento\Store\Model\Store::DEFAULT_STORE_ID);
+
+            $this->resourceConfig->saveConfig('blindhash/general/encryption_available', false, \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT, \Magento\Store\Model\Store::DEFAULT_STORE_ID);
+            $this->messageManager->addError(__('Api key is not valid.'));
+            return;
+        }
+
+        $encryptTest = $taplink->encryptTest();
+        $this->resourceConfig->saveConfig('blindhash/general/encryption_available', $encryptTest, \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT, \Magento\Store\Model\Store::DEFAULT_STORE_ID);
+
+        if ($encryptTest) {
             $this->resourceConfig->saveConfig('blindhash/general/api_public_key', $publicKey, \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT, \Magento\Store\Model\Store::DEFAULT_STORE_ID);
+        } else {
+            $this->resourceConfig->saveConfig('blindhash/general/enabled', '', \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT, \Magento\Store\Model\Store::DEFAULT_STORE_ID);
+            $this->resourceConfig->saveConfig('blindhash/general/api_public_key', '', \Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT, \Magento\Store\Model\Store::DEFAULT_STORE_ID);
         }
     }
 }
